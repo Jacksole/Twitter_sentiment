f0d4397a64ce985b6ed86d03e7d820fe394430d3
Le Aundre <leaundre.jackson87@gmail.com>
2020-05-05 17:29:51 -0400

    Update Files
    Added a search term to the index.js

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const Twitter = require('twitter-lite');
 
-const user = new Twitter({
-	    consumer_key: "wmDdd2eMCA3eT1e19HqAlzFWT",
-	    consumer_secret: "dZWdIlJYaEaweq0z2HJyFRlx2BpUSDVXNQQjwtX7KQChvxQ20C",
+(async function (){
+	const user = new Twitter({
+		consumer_key: "wmDdd2eMCA3eT1e19HqAlzFWT",
+		consumer_secret: "dZWdIlJYaEaweq0z2HJyFRlx2BpUSDVXNQQjwtX7KQChvxQ20C",
+	});
 	
-});
-
-// Wrap the following code in an async function that is called
-// immediately so that we can use "await" statements.
-(async function() {
 	try {
-		// Retrieve the bearer token from twitter.
-		const response = await user.getBearerToken();
-		console.log(`Got the following Bearer token from Twitter: ${response.access_token}`);
-                       
-               // Construct our API client with the bearer token.
-              const app = new Twitter({
-		      bearer_token: response.access_token,
-	      });
-	} catch(e) {
-		console.log("There was an error calling the Twitter API.");
+		let response = await user.getBearerToken();
+		const app = new Twitter({
+			bearer_token: response.access_token,
+		});
+		
+		// Search for recent tweets from the twitter API
+		response = await app.get(`/search/tweets`, {
+			q: "Lionel Messi", // The search term
+			lang: "en",        // Let's only get English tweets
+			cout: 100,         // Limit the results to 100 tweets
+		});
+
+		// Loop over all the tweets and print the text
+		for (tweet of response.statuses) {
+			console.dir(tweet.txt);
+		}
+	} catch(e){
+		console.log("There was an error calling the Twitter API");
 		console.dir(e);
 	}
 })();
